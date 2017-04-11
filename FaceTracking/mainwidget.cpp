4db95140f8e25e16b92c2b8a31624ee38d772c26
lchy1037 <270048522@qq.com>
2017-04-11 20:17:32 +0800

    update

--- FaceTracking/mainwidget.cpp
+++ FaceTracking/mainwidget.cpp
@@ -4,29 +4,41 @@ MainWidget::MainWidget(QWidget *parent)
     : QWidget(parent)
 {
     setWindowTitle("Face Detection And Tracking");
-//    showMaximized();
 
     local_video_radio = new QRadioButton("本地视频");
     camera_radio = new QRadioButton("摄像头");
     url_lineedit = new QLineEdit();
+    url_lineedit->setPlaceholderText("请选择视频来源");
+    start_btn = new QPushButton("开始");
     video_label = new QLabel();
-//    video_label->setPixmap(QPixmap(":/bg/bg1"));
-//    video_label->setScaledContents(true);
+    QPixmap pixmap1(":/bg/bg1");
+    pixmap1 = pixmap1.scaled(QSize(720, 1080), Qt::KeepAspectRatio);
+//    video_label->setGeometry(10, 20, 64, 64);
+    video_label->setPixmap(pixmap1);
     face_info_table = new QTableWidget();
 
     SetMainWindowLayout();
+    SetConnect();
 }
 
 void MainWidget::SetMainWindowLayout()
 {
+    QHBoxLayout *radio_layout = new QHBoxLayout();
+    radio_layout->addStretch();
+    radio_layout->addWidget(local_video_radio);
+    radio_layout->addStretch();
+    radio_layout->addWidget(camera_radio);
+    radio_layout->addStretch();
+
     QHBoxLayout *url_layout = new QHBoxLayout();
-    url_layout->addWidget(local_video_radio);
-    url_layout->addStretch();
-    url_layout->addWidget(camera_radio);
-    url_layout->addStretch();
+    url_layout->addSpacing(20);
     url_layout->addWidget(url_lineedit);
+    url_layout->addSpacing(20);
+    url_layout->addWidget(start_btn);
+    url_layout->addSpacing(20);
 
     QVBoxLayout *left_layout = new QVBoxLayout();
+    left_layout->addLayout(radio_layout);
     left_layout->addLayout(url_layout);
     left_layout->addWidget(video_label);
 
@@ -36,6 +48,20 @@ void MainWidget::SetMainWindowLayout()
     main_layout->addWidget(face_info_table);
 
     setLayout(main_layout);
+}
+
+void MainWidget::SetConnect()
+{
+    connect(local_video_radio, SIGNAL(toggled(bool)), this, SLOT(ChangeLineEditText(bool)));
+    connect(camera_radio, SIGNAL(toggled(bool)), this, SLOT(ChangeLineEditText(bool)));
+}
+
+void MainWidget::ChangeLineEditText(bool)
+{
+    if(local_video_radio->isChecked()) url_lineedit->setPlaceholderText("请输入本地视频路径");
+    if(camera_radio->isChecked()) url_lineedit->setPlaceholderText("请输入摄像头的地址");
+    if(!local_video_radio->isChecked() && !camera_radio->isChecked())
+        url_lineedit->setPlaceholderText("请选择视频来源");
 }
 
 MainWidget::~MainWidget()
--- FaceTracking/mainwidget.h
+++ FaceTracking/mainwidget.h
@@ -4,10 +4,13 @@
 #include <QWidget>
 #include <QLabel>
 #include <QLineEdit>
+#include <QPushButton>
 #include <QPixmap>
 #include <QRadioButton>
 #include <QTableWidget>
 #include <QHBoxLayout>
+//#include <cv.h>
+//#include <highgui.h>
 
 class MainWidget : public QWidget
 {
@@ -17,13 +20,19 @@ public:
     MainWidget(QWidget *parent = 0);
     ~MainWidget();
     void SetMainWindowLayout();
+    void SetConnect();
 
 private:
     QRadioButton *local_video_radio;
     QRadioButton *camera_radio;
     QLineEdit *url_lineedit;
+    QPushButton *start_btn;
     QLabel *video_label;
     QTableWidget *face_info_table;
+
+private slots:
+    void ChangeLineEditText(bool);
+
 };
 
 #endif // MAINWIDGET_H
